ae3785b4414895780c5c609d381f0415d127b9d2
Samuel <clooner0007@gmail.com>
2017-02-04 04:46:47 +0330

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#*****************************************************************************
+#* Choose "Minimal Server" during the intstallation (works With Minimal ISO)
+#*****************************************************************************
+. "$(dirname $(readlink -m $0))/lib/app.sh"
+
+#https://github.com/mafia-007
