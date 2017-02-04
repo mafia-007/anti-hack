36fe5676797098c488781394cf84c15a46a64f7c
Samuel <clooner0007@gmail.com>
2017-02-04 04:42:41 +0330

    Create F_01_check_os.sh

---
+++ functions/F_01_check_os.sh
@@ -0,0 +1,16 @@
+COMMANDS=(chfn chsh login ls passwd ps top netstat ifconfig killall pidof find)
+for com in ${COMMANDS[@]}
+do
+  COMM="$(which $com)"
+  echo "===${COMM}==="
+  BUGS="$(rpm -Vf $COMM)"
+  if [ -z "${BUGS}" ]
+  then
+    echo "PASS"
+  else
+    echo "Command \"${COMM}\" is hacked"
+    echo "${BUGS}"
+  fi
+  echo ""
+done
+#https://github.com/mafia-007
